f262c9e93e4d925ac18e4a0e09e6e42255f2f651
Gabriell <you@example.com>
2024-09-03 21:51:09 -0300

    commits da aula 4/5

--- Projeto2/todos/views.py
+++ Projeto2/todos/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Todo
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 
-def home(request):
-    return render(request, "todos/home.html")
+#def home(request):
+#    return render(request, "todos/home.html")
+
+#def todo_list(request):
+#    nome = 'Rodrigo'
+#    alunos = ['1. Ana', '2. José', '3. Bia']
+#    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
+
+
+
+#def todo_list_old(request):
+#    nome = 'Rodrigo'
+#    alunos = ['1. Ana', '2. José', '3. Bia']
+#    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
