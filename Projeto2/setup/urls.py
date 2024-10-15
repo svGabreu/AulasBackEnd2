e0a37426809e29cb4e67abd83571040cca0da2d2
Gabriell <you@example.com>
2024-10-15 20:58:32 -0300

    atualização de banco de dados

--- Projeto2/setup/urls.py
+++ Projeto2/setup/urls.py
@@ -19,7 +19,7 @@ from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDelete
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
-    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("create/<int:pk>", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
     path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
 ]
