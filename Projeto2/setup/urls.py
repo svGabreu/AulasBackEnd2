1f13d2dccf450a6b4f23804f72d5e5fe5b404702
Gabriell <you@example.com>
2024-10-15 20:16:25 -0300

    corrigindo problema do bd

--- Projeto2/setup/urls.py
+++ Projeto2/setup/urls.py
@@ -1,23 +1,25 @@
-
 from django.contrib import admin
 from django.urls import path
-#from todos.views import home
-#from todos.views import todo_list
-from todos.views import (TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView)
+
+# from todos.views import home
+# from todos.views import todo_list
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 
-#urlpatterns = [path("admin/", admin.site.urls), path("", home)]
+# urlpatterns = [path("admin/", admin.site.urls), path("", home)]
 
-#urlpatterns = [path("admin/", admin.site.urls), path("", todo_list)]
+# urlpatterns = [path("admin/", admin.site.urls), path("", todo_list)]
 
-#urlpatterns = [path("admin/", admin.site.urls), path("", TodoListView.as_view())]
+# urlpatterns = [path("admin/", admin.site.urls), path("", TodoListView.as_view())]
 
-#urlpatterns = [path("admin/", admin.site.urls),
+# urlpatterns = [path("admin/", admin.site.urls),
 #    path("", TodoListView.as_view()),
 #    path("create", TodoCreateView.as_view())]
 
-urlpatterns = [path("admin/", admin.site.urls),
- path("", TodoListView.as_view(), name="todo_list"),
- path("create", TodoCreateView.as_view(), name="todo_create"),
- path("update/<int:pk>",TodoUpdateView.as_view(),name="todo_update"),
- path("delete/<int:pk>",TodoDeleteView.as_view(),name="todo_delete")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+]
--- Projeto2/todos/views.py
+++ Projeto2/todos/views.py
@@ -6,43 +6,42 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
-#def home(request):
+# def home(request):
 #    return render(request, "todos/home.html")
 
-#def todo_list(request):
+# def todo_list(request):
 #    nome = 'Rodrigo'
 #    alunos = ['1. Ana', '2. José', '3. Bia']
 #    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
 
 
-
-#def todo_list_old(request):
+# def todo_list_old(request):
 #    nome = 'Rodrigo'
 #    alunos = ['1. Ana', '2. José', '3. Bia']
 #    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+
+# def todo_list(request):
+#     todos = Todo.objects.all()
+#     return render(request, "todos/todo_list.html", {"todos": todos})
+
 
 class TodoListView(ListView):
     model = Todo
 
-class TodoCreateView(CreateView):
-    model = Todo
-    fields = ["title", "deadline"]
-    success_url = reverse_lazy("todo_list")
 
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
 
 class TodoUpdateView(UpdateView):
-    model=Todo
-    fields=["title","deadline"]
-    success_url=reverse_lazy("todo_list")
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
 
 class TodoDeleteView(DeleteView):
-    model=Todo
-    success_url=reverse_lazy("todo_list")
+    model = Todo
+    success_url = reverse_lazy("todo_list")
